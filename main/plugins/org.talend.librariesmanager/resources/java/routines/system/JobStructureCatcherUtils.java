c68419a4e2e53da0b635de796245eacf30bc4031
wang wei <wwang@talend.com>
2020-09-18 15:06:44 +0800

    fix(TDI-44623): [OBSERVABILITY] : add PID and (Thread-ID OR UUID) information (#3614) (#3662)
    * fix(TDI-44623): [OBSERVABILITY] : add PID and (Thread-ID OR UUID)
    information
    
    * decrease the expensive call number for getting pid
    
    * fix(TDI-44807): Observability: initialize moment only when necessary
    
    * fix(TDI-44806): Observability: Add customization for event frequency

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/JobStructureCatcherUtils.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/JobStructureCatcherUtils.java
@@ -21,17 +21,19 @@
 // ============================================================================
 package routines.system;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 //TODO split to several classes by the level when have a clear requirement or design : job, component, connection
 public class JobStructureCatcherUtils {
 
-	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
 
 	// TODO split it as too big, even for storing the reference only which point
 	// null
@@ -59,8 +61,6 @@ public class JobStructureCatcherUtils {
 
 		public String job_version;
 
-		public Long systemPid = JobStructureCatcherUtils.getPid();
-
 		public boolean current_connector_as_input;
 
 		public String current_connector_type;
@@ -84,11 +84,17 @@ public class JobStructureCatcherUtils {
 
 		public long end_time;
 
-		public String moment = sdf.format(new Date());
+		public String moment;
 
 		public String status;
 		
 		public LogType log_type;
+		
+		//process uuid
+		public String pid = ProcessIdAndThreadId.getProcessId();
+				
+		//thread uuid
+		public String tid = ProcessIdAndThreadId.getThreadId();
 
 		public JobStructureCatcherMessage() {
 		}
@@ -202,6 +208,8 @@ public class JobStructureCatcherUtils {
 
 	public void addJobStartMessage() {
 		JobStructureCatcherMessage scm = new JobStructureCatcherMessage();
+		scm.moment = sdf.format(new Date());
+		
 		scm.job_name = this.job_name;
 		scm.job_id = this.job_id;
 		scm.job_version = this.job_version;
@@ -213,6 +221,8 @@ public class JobStructureCatcherUtils {
 
 	public void addJobEndMessage(long start_time, long end_time, String status) {
 		JobStructureCatcherMessage scm = new JobStructureCatcherMessage();
+		scm.moment = sdf.format(new Date());
+		
 		scm.job_name = this.job_name;
 		scm.job_id = this.job_id;
 		scm.job_version = this.job_version;
@@ -235,16 +245,6 @@ public class JobStructureCatcherUtils {
 			messages.clear();
 		}
 		return messagesToSend;
-	}
-
-	public static long getPid() {
-		RuntimeMXBean mx = ManagementFactory.getRuntimeMXBean();
-		String[] mxNameTable = mx.getName().split("@");
-		if (mxNameTable.length == 2) {
-			return Long.parseLong(mxNameTable[0]);
-		} else {
-			return Thread.currentThread().getId();
-		}
 	}
 
 	public void addConnectionMessage4PerformanceMonitor(String current_connector, String sourceId, String sourceLabel,
