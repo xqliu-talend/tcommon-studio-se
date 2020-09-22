7ce1c256ddcf1048fea41a683774e10d8725d685
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2020-09-22 21:32:23 +0800

    feat(TUP-28666): Extract TalendTextUtils (#3648)
    * feat(TUP-28666): Extract TalendTextUtils
    
    * feat(TUP-28666): call new TalendTextUtils
    
    * feat(TUP-28666): fix removeQuotesIfExist
    
    * feat(TUP-28666): rename TalendTextUtils

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
@@ -462,19 +462,25 @@ public class TalendTextUtils {
         return TalendQuoteUtils.removeQuotes(text);
     }
 
+    /**
+     * @Deprecated use org.talend.utils.string.StudioContextUtils.removeQuotesIfExist instead
+     */
+    @Deprecated
     public static String removeQuotesIfExist(String text) {
-        return TalendQuoteUtils.removeQuotesIfExist(text);
+        return org.talend.utils.string.StudioContextUtils.removeQuotesIfExist(text,
+                org.talend.utils.string.StudioContextUtils.QUOTATION_MARK);
     }
 
     /**
-     * qzhang Comment method "removeQuotes".
+     * @Deprecated use org.talend.utils.string.StudioContextUtils.removeQuotes
      *
      * @param text
      * @param quotation_mark2
      * @return
      */
+    @Deprecated
     public static String removeQuotes(String text, String quotation) {
-        return TalendQuoteUtils.removeQuotes(text, quotation);
+        return org.talend.utils.string.StudioContextUtils.removeQuotes(text, quotation);
     }
 
     public static boolean isEnclosed(String text) {
@@ -489,41 +495,12 @@ public class TalendTextUtils {
         return TalendQuoteUtils.getStringDeclare();
     }
 
+    /**
+     * @Deprecated use org.talend.utils.string.StudioContextUtils.encodeValue instead
+     */
+    @Deprecated
     public static String trimParameter(String value) {
-        if (value == null) {
-            return null;
-        }
-        int length = value.length();
-        String result = removeQuotes(value);
-        if (length > 1
-                && (((value.startsWith("\"") && value.endsWith("\""))) || (value.startsWith("\'") && value.endsWith("\'")))) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-            result = value.substring(1, length - 1);
-
-            if (result.contains("\\")) { //$NON-NLS-1$
-                result = result.replaceAll("\\\\n", "\n"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\b", "\b"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\f", "\f"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\r", "\r"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\t", "\t"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\\"", "\""); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\\\\\", "\\\\"); //$NON-NLS-1$ //$NON-NLS-2$
-
-            	// handle unicode
-                if (result.contains("\\u")) {
-                	for (int indexStart = 0; result.indexOf("\\u", indexStart) >= 0; indexStart = result.indexOf("\\u", indexStart)) {
-                		if (result.indexOf("\\u", indexStart) + 5 <= result.length()) { //$NON-NLS-1$
-                			int unicodeStart = result.indexOf("\\u"); //$NON-NLS-1$
-                			int unicodeEnd = unicodeStart + 5;
-                			result = result.substring(0, Math.max(0, unicodeStart))
-                					+ StringEscapeUtils.unescapeJava(result.substring(unicodeStart, unicodeEnd + 1))
-                					+ result.substring(Math.min(unicodeEnd + 1, result.length()), result.length());
-                		}
-	                }
-                }
-            }
-        }
-
-        return result;
+        return org.talend.utils.string.StudioContextUtils.encodeValue(value);
     }
 
     public static String getQuoteChar() {
