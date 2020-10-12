f815b154766a32ec28a60f83af8fe2ba2c75517e
jiezhang-tlnd <40561780+jiezhang-tlnd@users.noreply.github.com>
2020-10-12 16:47:33 +0800

    fix(TUP-28839)NPE when retrieve schema for JDBC connection of postgresql (#3700)
    https://jira.talendforge.org/browse/TUP-28839

--- main/plugins/org.talend.metadata.managment/src/main/java/org/talend/metadata/managment/model/DBConnectionFillerImpl.java
+++ main/plugins/org.talend.metadata.managment/src/main/java/org/talend/metadata/managment/model/DBConnectionFillerImpl.java
@@ -676,8 +676,14 @@ public class DBConnectionFillerImpl extends MetadataFillerImpl<DatabaseConnectio
         String catalogName = getDatabaseName(dbConn);
 
         if (StringUtils.isEmpty(catalogName)) {
+            String url = null;
+            if (metaConnection != null) {
+                url = metaConnection.getUrl();
+            } else {
+                url = dbConn.getURL();
+            }
             // TDQ-16020 msjian: should get the correct catalog name
-            catalogName = getPostgresqlCatalogFromUrl(metaConnection.getUrl(), dbConn.getUsername());
+            catalogName = getPostgresqlCatalogFromUrl(url, dbConn.getUsername());
         }
 
         if (StringUtils.isNotEmpty(catalogName)) {
