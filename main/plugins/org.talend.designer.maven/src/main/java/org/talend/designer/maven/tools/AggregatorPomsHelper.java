421ff918d1eaee9891bc49054145361ae0511be9
Mike Yan <yyan@talend.com>
2020-11-02 14:49:45 +0800

    fix(TESB-30556):[CI] SOAP service operation job is duplicated in the reactor

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/AggregatorPomsHelper.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/AggregatorPomsHelper.java
@@ -52,6 +52,7 @@ import org.eclipse.swt.widgets.Display;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.core.GlobalServiceRegister;
+import org.talend.core.IESBService;
 import org.talend.core.ILibraryManagerService;
 import org.talend.core.context.Context;
 import org.talend.core.context.RepositoryContext;
@@ -344,12 +345,8 @@ public class AggregatorPomsHelper {
     private static boolean checkIfCanAddToParentModules(Property property, boolean checkFilter) {
         // Check relation for ESB service job, should not be added into main pom
         if (property != null) {
-            List<Relation> relations = RelationshipItemBuilder.getInstance().getItemsRelatedTo(property.getId(),
-                    property.getVersion(), RelationshipItemBuilder.JOB_RELATION);
-            for (Relation relation : relations) {
-                if (RelationshipItemBuilder.SERVICES_RELATION.equals(relation.getType())) {
-                    return false;
-                }
+            if (isSOAPServiceProvider(property)) {
+                return false;
             }
 
             // for import won't add for exclude option
@@ -896,7 +893,7 @@ public class AggregatorPomsHelper {
                     }
                     IFile pomFile = getItemPomFolder(item.getProperty()).getFile(TalendMavenConstants.POM_FILE_NAME);
                     // filter esb data service node
-                    if (!isDataServiceOperation(object.getProperty()) && pomFile.exists()) {
+                    if (!isSOAPServiceProvider(object.getProperty()) && pomFile.exists()) {
                         modules.add(getModulePath(pomFile));
                     }
                 }
@@ -937,13 +934,21 @@ public class AggregatorPomsHelper {
      * @param property
      * @return
      */
-    private boolean isDataServiceOperation(Property property) {
+    private static boolean isSOAPServiceProvider(Property property) {
         if (property != null) {
             List<Relation> relations = RelationshipItemBuilder.getInstance().getItemsRelatedTo(property.getId(),
                     property.getVersion(), RelationshipItemBuilder.JOB_RELATION);
             for (Relation relation : relations) {
                 if (RelationshipItemBuilder.SERVICES_RELATION.equals(relation.getType())) {
                     return true;
+                }
+            }
+            if (GlobalServiceRegister.getDefault().isServiceRegistered(IESBService.class)) {
+                IESBService service = GlobalServiceRegister.getDefault().getService(IESBService.class);
+                if (service != null) {
+                    if (service.isSOAPServiceProvider(property.getItem())) {
+                        return true;
+                    }
                 }
             }
         }
