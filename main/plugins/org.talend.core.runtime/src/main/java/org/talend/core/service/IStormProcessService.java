32cac433d6e40f5ab050f818fb1d72051a8f0ed1
hzhao-talendbj <hzhao@talend.com>
2020-11-11 14:56:53 +0800

    remove storm

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IStormProcessService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IStormProcessService.java
@@ -14,17 +14,10 @@ package org.talend.core.service;
 
 import java.util.List;
 
-import org.eclipse.ui.IEditorPart;
-import org.talend.commons.ui.runtime.image.IImage;
 import org.talend.core.IService;
 import org.talend.core.model.components.IComponentsHandler;
-import org.talend.core.model.general.Project;
-import org.talend.core.model.process.INode;
 import org.talend.core.model.process.IProcess2;
 import org.talend.core.model.properties.Item;
-import org.talend.core.model.properties.ProcessItem;
-import org.talend.core.model.repository.IRepositoryEditorInput;
-import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.nodes.IProjectRepositoryNode;
 
@@ -33,17 +26,9 @@ import org.talend.repository.model.nodes.IProjectRepositoryNode;
  */
 public interface IStormProcessService extends IService {
 
-    boolean needStormProcess();
-
     IRepositoryNode getRootNode(IProjectRepositoryNode projectNode);
 
     public boolean collectStandardProcessNode(List<String> filteredContents, Object node);
-
-    public boolean isStormItem(Item item);
-
-    public boolean isStormEditor(IEditorPart editorPart);
-
-    public List<IRepositoryViewObject> getStormProcesses(Project project);
 
     /**
      * This method is responsible for creating additional information which are going to be registered in the Process
@@ -53,14 +38,9 @@ public interface IStormProcessService extends IService {
      */
     public void generateSparkStreamingInfosParameter(IProcess2 process);
 
-    public IImage getStormWizardIcon(String framework);
-
     public IComponentsHandler getSparkStreamingComponentsHandler();
 
     public boolean isSparkStreaming(Item item);
-
-    public IRepositoryEditorInput createTestCaseStormProcessEditorInput(ProcessItem processItem, boolean load,
-            String originalJobID, List<INode> testNodes, Boolean lastVersion, Boolean readonly);
 
     public IProcess2 createBigdataProcess(Item item);
 
