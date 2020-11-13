bb380767e6d0558d1323eaeae7deba8e893eb938
hzhao-talendbj <hzhao@talend.com>
2020-11-13 15:21:07 +0800

    add override method to remove db name

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/metadata/query/generator/PostgreQueryGenerator.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/metadata/query/generator/PostgreQueryGenerator.java
@@ -13,6 +13,7 @@
 package org.talend.core.model.metadata.query.generator;
 
 import org.talend.core.database.EDatabaseTypeName;
+import org.talend.core.model.metadata.QueryUtil;
 import org.talend.core.model.metadata.query.AbstractQueryGenerator;
 import org.talend.core.model.process.IElement;
 
@@ -37,5 +38,24 @@ public class PostgreQueryGenerator extends AbstractQueryGenerator {
             return schema;
         }
         return super.getSchema(elem);
+    }
+
+    @Override
+    protected String getTableNameWithDBAndSchema(final String dbName, final String schema, String tableName) {
+        if (tableName == null || EMPTY.equals(tableName.trim())) {
+            tableName = QueryUtil.DEFAULT_TABLE_NAME;
+        }
+
+        final StringBuffer tableNameWithDBAndSchema = new StringBuffer();
+
+        // postgres do not support db.schema.table_name, so only need schema.table_name
+        if (schema != null && !EMPTY.equals(schema)) {
+            tableNameWithDBAndSchema.append(checkContextAndAddQuote(schema));
+            tableNameWithDBAndSchema.append(getSQLFieldConnector());
+        }
+        //
+        tableNameWithDBAndSchema.append(checkContextAndAddQuote(tableName));
+
+        return tableNameWithDBAndSchema.toString();
     }
 }
